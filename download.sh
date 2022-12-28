0c278c5a4642f83721ddfb7d7fad0cedcd77eeae
zhi_zed <76503635+didi-zhiyuan@users.noreply.github.com>
2022-12-29 00:29:25 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -9,20 +9,20 @@ download(){
 echo "Downloading binaries..."
 
 echo "Downloading kernel 2.6 (MIPS)..."
-download https://github.com/firmadyne/kernel-v2.6/releases/download/v1.1/vmlinux.mipsel
-download https://github.com/firmadyne/kernel-v2.6/releases/download/v1.1/vmlinux.mipseb
+download https://ghproxy.com/https://github.com/firmadyne/kernel-v2.6/releases/download/v1.1/vmlinux.mipsel
+download https://ghproxy.com/https://github.com/firmadyne/kernel-v2.6/releases/download/v1.1/vmlinux.mipseb
 
 echo "Downloading kernel 4.1 (ARM)..."
-download https://github.com/firmadyne/kernel-v4.1/releases/download/v1.1/zImage.armel
+download https://ghproxy.com/https://github.com/firmadyne/kernel-v4.1/releases/download/v1.1/zImage.armel
 
 echo "Downloading console..."
-download https://github.com/firmadyne/console/releases/download/v1.0/console.armel
-download https://github.com/firmadyne/console/releases/download/v1.0/console.mipseb
-download https://github.com/firmadyne/console/releases/download/v1.0/console.mipsel
+download https://ghproxy.com/https://github.com/firmadyne/console/releases/download/v1.0/console.armel
+download https://ghproxy.com/https://github.com/firmadyne/console/releases/download/v1.0/console.mipseb
+download https://ghproxy.com/https://github.com/firmadyne/console/releases/download/v1.0/console.mipsel
 
 echo "Downloading libnvram..."
-download https://github.com/firmadyne/libnvram/releases/download/v1.0c/libnvram.so.armel
-download https://github.com/firmadyne/libnvram/releases/download/v1.0c/libnvram.so.mipseb
-download https://github.com/firmadyne/libnvram/releases/download/v1.0c/libnvram.so.mipsel
+download https://ghproxy.com/https://github.com/firmadyne/libnvram/releases/download/v1.0c/libnvram.so.armel
+download https://ghproxy.com/https://github.com/firmadyne/libnvram/releases/download/v1.0c/libnvram.so.mipseb
+download https://ghproxy.com/https://github.com/firmadyne/libnvram/releases/download/v1.0c/libnvram.so.mipsel
 
 echo "Done!"
